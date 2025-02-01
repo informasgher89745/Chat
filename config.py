33f8a0da930944bbf8af2e920b4bc11ee9e3e6c5
informasgher89745 <informasgher+t@gmail.com>
2025-02-02 04:32:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = "21064438"
+API_HASH = "052d2f50aac2fe3e1d3047be2ec38f31"
+BOT_TOKEN = "8003594750:AAFKvwSje60zUxiSHwVhys-B0TYKMgQOQ18"
+MONGO_URL = "mongodb+srv://venturepvtme:INc7HJsa1gt9oZ33@lgcyalex855101.kgbzc.mongodb.net"
+BOT_USERNAME = "GrehaniChatBot"
+BOT_NAME = "Grehani"
+
